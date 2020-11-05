768401236e6f97a1e3fb055558331bbd5ca2bba9
bysir <zbysir@qq.com>
2020-11-05 22:21:15 +0800

    UI ing

--- src/views/create/Index.vue
+++ src/views/create/Index.vue
@@ -3,13 +3,14 @@
   <div class="main-area">
     <el-steps :active="step" align-center>
       <el-step title="选择场景"></el-step>
-      <el-step title="选择主题"></el-step>
+      <el-step title="选择模板"></el-step>
       <el-step title="创建站点"></el-step>
       <el-step title="完成"></el-step>
     </el-steps>
 
     <div v-if="step!==1">
       <el-button
+        class="go-back"
         round
         @click="goBack"
         type="text">
@@ -50,16 +51,41 @@
         </div>
       </div>
     </div>
-    <div v-else-if="step===2">
-      选择主题
-      <div>
-        <el-button @click="toNext">下一步</el-button>
-      </div>
+    <div class="choose-tpl" v-else-if="step===2">
+      <h3 class="title">选择模板</h3>
+      <el-row class="tpl-list" :gutter="20">
+        <el-col class="tpl-item"
+                v-for="i in 10"
+                :key="i"
+                :span="6">
+          <div class="tpl-item__inner">
+            <div class="tpl-item__thumbnail">
+              <img src="https://www.baklib.com/templates/help-scout/1/assets/images/1200x750.png" alt="">
+            </div>
+            <div class="tpl-item__info">
+              <div class="tpl-item__name">
+                模板名称
+              </div>
+              <div class="tpl-item__choose">
+                <el-button
+                  type="primary"
+                  @click="toNext"
+                  size="small">选择模板
+                </el-button>
+              </div>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
     </div>
-    <div v-else-if="step===3">
-      3
-      <div>
-        <el-button @click="toNext">下一步</el-button>
+    <div class="site-info" v-else-if="step===3">
+      <h3>站点名称</h3>
+      <div class="site-name">
+        <el-input placeholder="请输入名称"></el-input>
+      </div>
+
+      <div class="step-btn">
+        <el-button round type="primary" @click="toNext">下一步</el-button>
       </div>
     </div>
     <div v-else>
@@ -153,4 +179,66 @@
     }
   }
 
+  .step-btn {
+    margin-top: 20px;
+
+    .el-button {
+      padding: 12px 60px;
+    }
+  }
+
+  .site-info {
+    margin: 30px auto 0 auto;
+    text-align: center;
+    max-width: 800px;
+  }
+
+  .site-name {
+    margin-top: 20px;
+
+    /deep/ .el-input__inner {
+      border: none;
+      border-bottom: 1px solid #C0C4CC;
+      border-radius: 0;
+    }
+  }
+
+  .go-back {
+    color: #666;
+  }
+
+  .choose-tpl {
+    .title {
+      text-align: center;
+    }
+  }
+
+  .tpl-list {
+
+  }
+
+  .tpl-item {
+    padding-bottom: 20px;
+  }
+
+  .tpl-item__inner {
+    border: 1px solid #ebeef5;
+    /*margin: 10px;*/
+
+    &:hover {
+      box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .1);
+    }
+  }
+
+  .tpl-item__thumbnail {
+    img {
+      width: 100%;
+    }
+  }
+
+  .tpl-item__info {
+    display: flex;
+    padding: 10px;
+    justify-content: space-between;
+  }
 </style>
